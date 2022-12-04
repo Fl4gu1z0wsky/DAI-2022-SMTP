ba39441cdd593261a4012fb4ae7520782cfeb995
Flag <flagui1462@hotmail.com>
2022-12-04 15:11:58 +0100

    New captures + comments on Constants.java

--- src/DAI_SMTP/Constants.java
+++ src/DAI_SMTP/Constants.java
@@ -7,6 +7,8 @@ package DAI_SMTP;
 public class Constants {
     public  static final String DisplaySeparator = "=================================================";
     public static int MinimumMailPerGroup = 2;
+    // Change if the server is not local
     public static String ServerAddress = "localhost";
+    //Do not change
     public static int ServerPort = 25;
 }
